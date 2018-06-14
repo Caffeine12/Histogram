f8714367e493d789a1f438e20c341c23bdac5161
Jubair Obhi <jubairku12@gmail.com>
2018-06-14 23:29:21 +0600

    An RGB image has been converted to binary image

---
+++ histogram.m
@@ -0,0 +1,26 @@
+%Here I will read image file from UI
+[fn pn] = uigetfile('*.png', 'Select image file');
+filepath= strcat(pn,fn);
+image = imread(filepath);
+%Read red green and blue pixel values
+red = double(image(:,:,1));
+green = double(image(:,:,2));
+blue = double(image(:,:,3));
+%Took average of red green and blue values of pixels
+average = double((red+green+blue)/3);
+%Converting double matrix into integer matrix
+grayscale = uint8(average);
+%Converting grayscale image to binary image
+binary = grayscale;
+for i=1:size(grayscale,1)
+    for j=1:size(grayscale,2)
+        if(grayscale(i,j))>127
+            binary(i,j)=1;
+        else
+            binary(i,j)=0;
+        end
+    end
+end
+binary=single(binary);
+%Displaying the image in UI
+figure, imshow(binary);
